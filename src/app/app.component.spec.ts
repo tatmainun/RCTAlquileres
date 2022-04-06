6741b6da6f8eb99dd61cfbbe48655994e1d0f1a8
tatmainun <tatiana.mailen.nunes@gmail.com>
2022-04-06 14:57:06 -0300

    Llamo al servicio Reservar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ReservarService } from './servicios/reservar/reservar.service';
+
+class ReservarServiceMock {
+  reservarDepartamento() {
+    return true
+  }
+}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: ReservarService, useClass: ReservarServiceMock }
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -28,4 +44,13 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('rctalquileres app is running!');
   });
+
+  fit('se deberia llamar al servicio de reserva al querer reservar', () => {
+    spyOn(component, 'reservarDepto');
+    fixture.detectChanges();
+    expect(component.reservarDepto).toHaveBeenCalled();
+
+    // const reservarDepartamento = spyOn(ReservarService, 'reservarDepartamento').and.returnValue(true);
+    // expect(reservarDepartamento).toHaveBeenCalled();
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ReservarService } from './servicios/reservar/reservar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  @Input()
+  private fechaDesde: String = "";
+
+  @Input()
+  private fechaHasta: String = "";
+
+  constructor(private reservarService: ReservarService){}
+
+  ngOnInit(): void {
+    this.reservarDepto();
+  }
+
+  public reservarDepto():void {
+    this.reservarService.reservarDepartamento(this.fechaDesde, this.fechaHasta);
+  }
+
   title = 'rctalquileres';
 }
